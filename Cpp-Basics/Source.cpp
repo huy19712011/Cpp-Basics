745ff92753758f8241d72e413ef3799426322dc2
huy19712011 <huy19712011@gmail.com>
2021-10-04 12:13:39 +0700

    update some text

--- Cpp-Basics/Source.cpp
+++ Cpp-Basics/Source.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-	std::cout << "Hello World\t";
-	std::cout << "For test only";
-	std::cout << "after 3rd change on gitHub for test";
-	std::cout << "after 4th change on gitHub for test";
-	std::cout << "after 5th change on gitHub for test";
+	std::cout << "Hello World\n";
+	std::cout << "For test only\n";
+	std::cout << "after 3rd change on gitHub for test\n";
+	std::cout << "after 4th change on gitHub for test\n";
+	std::cout << "after 5th change on gitHub for test\n";
 }
 
 // one more test on gitHub
